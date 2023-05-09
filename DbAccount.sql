7320710a33fa8466b22c2f85556500577970efad
Sebastian Mattias Rodriguez <seba.rodri.29@gmail.com>
2023-05-09 09:48:08 +0200

    Change type data in date time on database

--- DbAccount.sql
+++ DbAccount.sql
@@ -5,5 +5,5 @@ create table Account(
 	idAccount int identity not null,
 	username varchar(50) not null primary key,
 	pwd varchar(50) not null,
-	date_of_creation date not null
+	date_of_creation datetime not null
 )
